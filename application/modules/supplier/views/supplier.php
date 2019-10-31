8b6e972cb8c6dba5a982233f962a462b86426d54
Rogen <rmpagsanjan@proweaver.net>
2019-10-31 17:47:51 +0800

    Rogen 5:47pm

--- application/modules/supplier/views/supplier.php
+++ application/modules/supplier/views/supplier.php
@@ -10,14 +10,14 @@
           ENROLLMENT > <span class="active1"> SUPPLIER </p>
         </div>
         <div class="col-sm-6">
-            <button  type="button"class="button1 float-sm-right" data-toggle="modal" data-target="#exampleModal "><i class="fas fa-plus-circle" aria-hidden="true"></i> Add User </button>
+            <button  type="button"class="button1 float-sm-right" data-toggle="modal" data-target="#exampleModal "><i class="fas fa-plus-circle" aria-hidden="true"></i> Add Supplier </button>
         </div>
         <!-- Modal -->
       <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Add Supplier</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -52,40 +52,92 @@
     </div><!-- /.container-fluid -->
   </section>
     <!-- Default box -->
-    <div class="main header">
-      <div class="col-md-12">
-        <nav>
-            <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-                <button class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">New Jiandra</button>
-                <button class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Mrs.P</button>
-            </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-              <table class="table" cellspacing="0">
+    <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body1">
+                <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                  <div class="row">
+                      <div class="col-sm-12 col-md-6">
+                        <div class="dataTables_length" id="example1_length">
+                          <label>Show
+                            <select name="example1_length" aria-controls="example1" class="custom-select custom-select-sm form-control form-control-sm">
+                            <option value="10">10</option>
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                          </select> entries
+                          </label>
+                        </div>
+                      </div>
+                  <div class="col-sm-12 col-md-6">
+                      <div id="example1_filter" class="dataTables_filter">
+                        <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
+                      </div>
+                  </div>
+                </div>
+                <div class="row">.
+                  <div class="col-sm-12">
+                    <table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
                   <thead>
-                      <tr>
-                          <th>Supplier Logo</th>
-                          <th>Supplier Name</th>
-                          <th>Action</th>
-                          <th>Status</th>
-                      </tr>
+                    <tr class="table-header" role="row">
+                      <th class="header-title sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 283px;">Supplier Logo</th>
+                      <th class="header-title sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 359px;">Supplier Name</th>
+                      <th class="header-title sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 320px;">Action</th>
+                      <th class="header-title sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 176px;">Status</th>
+                    </tr>
                   </thead>
                   <tbody>
-                      <tr>
-                        <td>Company Logo</td>
-                        <td>Rojs Gen</td>
-                        <td><i class="fa fa-clone" aria-hidden="true"></i><i class="fas fa-pen"></i><i class="fa fa-trash" aria-hidden="true"></i></td>
-                        <td><button type="button" class="active btn btn-block btn-success">active</button></td>
-                      </tr>
-                      <tr>
-                        <td>Company Logo</td>
-                        <td>Ariane</td>
-                        <td><i class="fa fa-clone" aria-hidden="true"></i><i class="fas fa-pen"></i><i class="fa fa-trash" aria-hidden="true"></i></td>
-                        <td><button type="button" class="inactive btn btn-block btn-danger">inactive</button></td>
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
-    </div>
-</div>
+
+                    <tr role="row" class="odd">
+                      <td class="table-data sorting_1">Gecko</td>
+                      <td class="table-data">1.7</td>
+                      <td class="action"><i class="fas fa-pen"></i> <i class="fa fa-trash" aria-hidden="true"></i></td>
+                      <td class="table-data"><button type="button" class="inactive btn btn-block btn-danger">inactive</button></td>
+                    </tr>
+                    <tr role="row" class="odd">
+                      <td class="sorting_1">Gecko</td>
+                      <td class="table-data">Firefox 1.0</td>
+                      <td class="table-data">1.7</td>
+                      <td class="action"><i class="fas fa-pen"></i> <i class="fa fa-trash" aria-hidden="true"></i></td>
+                      <td class="table-data"><button type="button" class="active btn btn-block btn-success">inactive</button></td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12 col-md-5">
+                  <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
+                </div>
+                <div class="col-sm-12 col-md-7">
+                  <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                    <ul class="pagination">
+                      <li class="paginate_button page-item previous disabled" id="example1_previous">
+                        <a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a>
+                      </li>
+                      <li class="paginate_button page-item active">
+                        <a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a>
+                      </li>
+                      <li class="paginate_button page-item ">
+                        <a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a>
+                      </li>
+                      <li class="paginate_button page-item ">
+                        <a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a>
+                      </li>
+                      <li class="paginate_button page-item ">
+                        <a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a>
+                      </li>
+                      <li class="paginate_button page-item ">
+                        <a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a>
+                      </li>
+                      <li class="paginate_button page-item next" id="example1_next">
+                        <a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
