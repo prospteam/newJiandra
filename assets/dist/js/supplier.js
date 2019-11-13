95b3459427cef2a178340470c89db27ff0c78b42
Rogen <rmpagsanjan@proweaver.net>
2019-11-13 17:00:15 +0800

    rogen V1 5:00

--- assets/dist/js/supplier.js
+++ assets/dist/js/supplier.js
@@ -1,7 +1,18 @@
       var base_url = $('input[name="base_url"]').val();
       $(document).ready(function(){
-        //display suppliers
-      // alert('lol');
+
+        $('.my-file').on('change',function(){
+
+          var myfile = this.value;
+          var str = myfile.split("\\");
+
+          if(str[str.length-1]){
+            $('.filechosen').html(str[str.length-1]);
+          }else{
+            $('.filechosen').html('No file chosen');
+          }
+        });
+
           $('.company1').on('click', function(e){
             e.preventDefault();
 
@@ -361,27 +372,29 @@
          type: 'post',
          dataType: 'json',
          success: function(data){
-           console.log(data);
            $('#editSupplier').modal('show');
            // console.log(data.view_edit.supplier_logo);
 
-             $('#editSupplier input[name=supplier_name]').val(data.supplier.supplier_name);
-             $('#editSupplier input[name=supplier_contact_person]').val(data.supplier.supplier_contact_person);
+             $('#editSupplier input[name =supplier_name]').val(data.view_edit.supplier_name);
+             $('#editSupplier input[name=supplier_contact_person]').val(data.view_edit.supplier_contact_person);
              var company_list = [];
              $('#editSupplier .js-example-basic-multiple-edit').html('');
-             $.each(data.company, function(key,val){
+             // var test = new Array('test','test2');
+
+             // Array.isArray(variable);
+             // $.each(test, function(key,val){
+               // alert(val);
                // company_list.push(val.company_name);
-               // console.log(val);
-               $('#editSupplier .js-example-basic-multiple-edit').append( '<option value='+val.company_id+' selected>'+val.company_name+'</option>' );
-             })
-             // $('#editSupplier select[name=company[]').val(data.view_edit.company);
-             $('#editSupplier input[name=office_number]').val(data.supplier.office_number);
-             $('#editSupplier input[name=home_number]').val(data.supplier.home_number);
-             $('#editSupplier input[name=mobile_number]').val(data.supplier.mobile_number);
-             $('#editSupplier input[name=tin_number]').val(data.supplier.tin_number);
-             $('#editSupplier input[name=fax_number]').val(data.supplier.fax_number);
-             $('#editSupplier input[type=file]').val('test');
-             // $('#editSupplier input[name=logo]').val(data.view_edit.supplier_logo);
+               // console.log(data.view_edit.company_name);
+               // $('#editSupplier .js-example-basic-multiple-edit').append( '<option value='+val.company_id+' selected>'+val.company_name+'</option>' );
+             // });
+             // $('#editSupplier select[name=company').val(company_list);
+             $('#editSupplier input[name=office_number]').val(data.view_edit.office_number);
+             $('#editSupplier input[name=home_number]').val(data.view_edit.home_number);
+             $('#editSupplier input[name=mobile_number]').val(data.view_edit.mobile_number);
+             $('#editSupplier input[name=tin_number]').val(data.view_edit.tin_number);
+             $('#editSupplier input[name=fax_number]').val(data.view_edit.fax_number);
+             $('#editSupplier input:file[name=logo]').val(data.view_edit.supplier_logo);
 
              // $('#editSupplier select[name=position]').val(data.view_edit.position);
          }
