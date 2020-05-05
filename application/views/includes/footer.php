c6559e9981461770798e4268ed46cf58dd87f6cd
Rogen <rmpagsanjan@proweaver.net>
2020-05-05 20:08:15 +0800

    Rogen May 5, 20/ Rogen

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -46,7 +46,7 @@
 <!-- STOCK RECEIVE -->
 <script src="<?php echo base_url('assets/dist/js/stockreceive.js'); ?>"></script>
 <!-- PRODUCTS IMPORT -->
-<script src="<?php echo base_url('assets/dist/js/product_import.js'); ?>"></script>
+<script src="<?php echo base_url('assets/dist/js/productsimport.js'); ?>"></script>
 <script src="<?php echo base_url('assets/dist/js/poimport.js'); ?>"></script>
 
 
